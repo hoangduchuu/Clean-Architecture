626af6b58a1fb16c023e8896eb23bb4be0fba736
Huu Hoang <hoangduchuuvn@gmail.com>
2018-11-23 20:53:31 +0700

    [DEV] Implementing the GetProjectUseCase
    WIthin Clean Architecture, the Domain layer defines Use Cases which define operations which can be performed in our application. In this lesson, we’ll be implementing the Get Projects Use Cases classes for our application so that we can retrieve a list of projects from the external data source through the domain layer of our Android project.
    
    In Clean Architecture, the Domain layer defines Use Cases which define operations which can be performed in our application. In this lesson, we’ll be implementing the Get Bookmarked Projects Use Cases class for our application so that we can retrieve a list of bookmarked projects within our external data source through our domain layer.

---
+++ Domain/src/main/java/grabteacher/com/domain/usecase/GetProjectUseCase.kt
@@ -0,0 +1,18 @@
+package grabteacher.com.domain.usecase
+
+import grabteacher.com.domain.executor.PostExecutionThread
+import grabteacher.com.domain.interactor.ObservableUseCase
+import grabteacher.com.domain.model.Project
+import grabteacher.com.domain.repository.ProjectRepository
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class GetProjectUseCase @Inject constructor(
+        private val projectRepository: ProjectRepository,
+        private val postExecutionThread: PostExecutionThread):ObservableUseCase<List<Project>, Nothing?>(postExecutionThread){
+
+    override fun buildUseCaseObservable(params: Nothing?): Observable<List<Project>> {
+        return projectRepository.getProjects()
+    }
+
+}
